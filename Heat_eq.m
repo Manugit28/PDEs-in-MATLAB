0fa8d1dbbe7406e0c9c87c06efd733b7da34af8a
Manugit28 <49473724+Manugit28@users.noreply.github.com>
2023-07-08 09:16:01 +0530

    Add files via upload
    Using Numerical Discretization schemes to implement PDEs in MATLAB

---
+++ Heat_eq.m
@@ -0,0 +1,80 @@
+clear;clc;
+% Heat Equation
+
+k=1;
+% Length of rod
+L=1;
+% dx is the 
+dx=1e-2;
+x=0:dx:L;
+
+dt=0.5*1e-4;
+stopTime=0.01;
+
+mx=(L/dx)+1;
+
+lambda=(dt*k^2)/(dx^2);
+
+t1=0:dt:stopTime;
+A=zeros(length(0:dt:stopTime),mx);
+% Initial conditions
+current=10*cos((2*pi/L).*x);
+% current=6*sin((pi/L).*x);
+fut=zeros(1,mx);
+
+A(1,:)=current;
+
+
+% Solution
+
+figure(1);
+plot(x,current,'r','Linewidth',2);
+hold on;
+
+i=2;
+
+
+for t=dt:dt:stopTime
+    
+
+    %Boundary conditions
+
+    fut(1)=0;
+    fut(mx)=0;
+
+    fut(2:mx-1)=(lambda*current(1:mx-2))+(1-2*lambda)*current(2:mx-1)+...
+        (lambda*current(3:mx));
+
+    %Swap the values for the next iteration
+
+    current=fut;
+    A(i,:)=fut;
+    i=i+1;
+
+    plot(x,fut,'b','Linewidth',3);
+    title({'1-D Heat Equation';...
+        ['time(\itt)=',num2str(t)]})
+    xlabel('Spatial co-ordinate (x) \rightarrow')
+    ylabel('Transport property profile (u) \rightarrow')
+    hold on;
+    grid on;
+    axis([0 L -10 10]);
+
+    hold off;
+    pause(0.01);
+end
+figure(2);
+surf(x,t1,A,'Edgecolor','none');colormap(jet);colorbar;
+axis square;
+%surf is for surface plot
+
+[X,T]=meshgrid(x,t1);
+
+U=6.*sin((pi/L).*X).*exp((-k.*T).*(pi./L).^2);
+
+%Analytical
+figure(3);
+surf(X,T,U,'Edgecolor','none');colormap(jet);colorbar;
+axis square;
+
+xlabel('$x$','Interpreter','latex','FontSize',20);
---
+++ wave.m
@@ -0,0 +1,85 @@
+%23rd March 2023
+%u_tt=a^2u_xx
+
+clear;clc;
+%Wave Equation
+%CTCS
+
+a=1;
+L=10;
+
+dx=1e-1;
+x=0:dx:L;
+mx=length(x);
+
+dt=0.5*1e-1;
+stopTime=20;
+t=0:dt:stopTime;
+mt=length(t);
+
+
+lambda=((dt^2)*(a^2))/(dx^2);
+
+
+%A=zeros(length(0:dt:stopTime),mx);
+%% Initial conditions
+current=(L/2)-(L/2)*cos((2*pi/L).*x);
+past=current;
+fut=zeros(1,mx);
+%fut=zeros(1,mx);
+
+A=zeros(mt,mx);
+A(1,:)=current;
+
+
+%% Solution
+
+figure(1);
+plot(x,current,'-r','Linewidth',2);
+hold on;
+
+for j=2:mt
+    
+
+    %Boundary conditions
+
+    fut(1)=0;
+    fut(mx)=0;
+
+    fut(2:mx-1)=(2*current(2:mx-1))-past(2:mx-1)+...
+        lambda*(current(1:mx-2)-2*current(2:mx-1)+current(3:mx));
+
+    %Swap the values for the next iteration
+
+    past=current;
+    current=fut;
+    A(j,:)=fut;
+   
+
+    plot(x,fut,'b','Linewidth',3);
+    title({'1-D Wave Equation';...
+        ['time(\itt)=',num2str((j-1)*dt)]})
+    xlabel('Spatial co-ordinate (x) \rightarrow')
+    ylabel('Transport property profile (u) \rightarrow')
+    hold on;
+    grid on;
+    axis([0 L -10 10]);
+
+    hold off;
+    pause(0.01);
+end
+figure(2);
+surf(x,t,A,'Edgecolor','none');colormap(jet);colorbar;
+axis square;
+%surf is for surface plot
+
+% [X,T]=meshgrid(x,t);
+% 
+% U=6.*sin((pi/L).*X).*exp((-k.*T).*(pi./L).^2);
+% 
+% %Analytical
+% figure(3);
+% surf(X,T,U,'Edgecolor','none');colormap(jet);colorbar;
+% axis square;
+% 
+% xlabel('$x$','Interpreter','latex','FontSize',20);
